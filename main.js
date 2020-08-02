78015cff86d596a5687dd3959d5925a94aea5d0d
RaySunlight <some.other.fairy@gmail.com>
2020-08-02 15:33:45 +0000

    cupcake

--- main.js
+++ main.js
@@ -1,102 +1,29 @@
+//cupcake function
 "use strict";
-
-// initializing the game engine
 const gameEngine = new GameEngine();
 
-/**
- * Run a single loop of the game.
- * @param {object}   data    the game state.
- */
+function cupcake_show(x, y, gameEngine) {
+  let position =[];
+
+  for (let i=0; i<10; i+=1) {
+    //the y
+    for (let j=0; j<10; j+=1) {
+      position.push([x+i, y+j]);
+      position.push([x-i, y+j]);
+    }
+
+    for (let j=0; j<10; j+=1) {
+      position.push([x+i, y-j]);
+      position.push([x-i, y-j]);
+    }
+  }
+  gameEngine.fillPixels(position, 170, 70, 210 , 255);
+}
 
 function mainLoop(data) {
-  let xclicker = gameEngine.getMouseX();
-  let yclicker = gameEngine.getMouseY();
-  let xpt = 0;
-  let ypt = 0;
-
-  while (xpt<1000){
-    while(ypt<1000){
-     if ((xpt - xclicker)^2+(ypt - yclicker)^2 === 2500){
-       gameEngine.fillPixels([[xpt, ypt]], 200, 0, 200,  255);
-      }
-     xpt+=1;
-    }
-   ypt+=1;
-  }
+  gameEngine.clear();
+  cupcake_show(100, 100, gameEngine);
+  
 }
 
 gameEngine.startMainLoop(mainLoop, {});
-
-
-/*
-let num = 100;
-  while (num<500){
-    gameEngine.fillPixels([[num,300]], 255, 0, 0, 255);
-    num+=1;
-  }
-
-let xclicker = getMouseX();
-let yclicker = getMouseY();
-let xpt = 0;
-let ypt = 0;
-
-while (xpt<1000){
-  while(ypt<1000){
-    if ((xpt - xclicker)^2+(ypt - yclicker)^2 === 2500){
-      gameEngine.fillPixels([[xpt, ypt]], 200, 0, 200,  255);
-    }
-    xpt+=1;
-  }
-  ypt+=1;
-  let num = 100;
-  while (num<500){
-    gameEngine.fillPixels([[num,300]], 255, 0, 0, 255);
-    num+=1;
-  }
-
-let xclicker = gameEngine.getMouseX();
-let yclicker = gameEngine.getMouseY();
-let xpt = 0;
-let ypt = 0;
-
-while (xpt<1000){
-  while(ypt<1000){
-    if ((xpt - xclicker)^2+(ypt - yclicker)^2 === 2500){
-      gameEngine.fillPixels([[xpt, ypt]], 200, 0, 200,  255);
-    }
-    xpt+=1;
-  }
-  ypt+=1;
-}
-}
-
-*/
-/*let arrx=[];
-  for(let i=0; i<500; i+=1)
-  {
-    arrx.push([i]);
-  }
-  let arry=[];
-  for(let j=10; j<350; j+=1)
-  {
-    arry.push([i]);
-  }
- for(let m=0; m<10; m++)
-  {
-   console.log(arrx[m]);
-  }
- 
-  for(let a=0; a<arrx.length; a+=1)
-  {
-    for(let b=0; b<arry.length; b+=1){
-      gameEngine.fillPixels([[arrx[a],arry[b]]],255, 0, 0, 255);
-    }
-  }*/
- 
- /* let num = 100;
-  while (num<500){
-    gameEngine.fillPixels([[num,300], [num ,301], [num,302], [num,303], [num ,304], [num,305]], 255, 0, 0, 255);
-    num+=1;
-  }
-  //gameEngine.playSound(chime);
-  */
