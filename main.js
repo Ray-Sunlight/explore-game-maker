bb6f46fc7cfb45f0b7e702d412ebd8360dd02c0b
RaySunlight <some.other.fairy@gmail.com>
2020-08-02 10:21:51 +0000

    things

--- main.js
+++ main.js
@@ -8,14 +8,21 @@ const gameEngine = new GameEngine();
  * @param {object}   data    the game state.
  */
 function mainLoop(data) {
-  let array=[];
-  mousex = gameEngine.getMouseX();
-  for (let i=1; i<200; ){
-    array.push(mousex+i);
+  let array1=[];
+  let mousex = getMouseX();
+  for (let i=1; i<200; i+=1){
+    array1.push(mousex+i);
   }
 
+  let array2=[];
+  let mousey = getMouseY();
+  for (let j=1; j<200; i+=1){
+    array2.push(mousey+i);
+  }
+  
+  gameEngine.playSound("badBoing");
 
-  gameEngine.fillPixels([[10,0], [10, 1], [10, 2], [10,3], [10, 4], [10, 5]], 255, 0, 0 , 255);
+  gameEngine.fillPixels([[0,1]], 255, 0, 0 , 255);
 
 }
 
