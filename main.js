a355c35c6237e88fa3752172469da079599cb0b6
RaySunlight <some.other.fairy@gmail.com>
2020-08-02 16:00:55 +0000

    includes delete cake

--- main.js
+++ main.js
@@ -17,13 +17,37 @@ function cupcake_show(x, y, gameEngine) {
       position.push([x-i, y-j]);
     }
   }
-  gameEngine.fillPixels(position, 170, 70, 210 , 255);
+
+  gameEngine.fillPixels(position, 170, 70, 210, 255);
+}
+
+function cupcake_delete(x, y, gameEngine) {
+  let position =[];
+
+  for (let i=0; i<10; i+=1) {
+    //the y
+    for (let j=0; j<10; j+=1) {
+      position.push([x+i, y+j]);
+      position.push([x-i, y+j]);
+    }
+
+    for (let j=0; j<10; j+=1) {
+      position.push([x+i, y-j]);
+      position.push([x-i, y-j]);
+    }
+  }
+
+  gameEngine.fillPixels(position, 0, 0, 0, 0);
+}
+
+let cupdinate = {
+  x:25,
+  y:25
 }
 
 function mainLoop(data) {
-  gameEngine.clear();
-  cupcake_show(100, 100, gameEngine);
-  
+  cupcake_show(cupdinate.x, cupdinate.y, gameEngine);
+
 }
 
 gameEngine.startMainLoop(mainLoop, {});
