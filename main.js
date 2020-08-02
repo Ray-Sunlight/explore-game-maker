2b8f7a1156980f68f41b7d0e6707020da23c3342
RaySunlight <some.other.fairy@gmail.com>
2020-08-02 09:44:11 +0000

    add pixel color

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const gameEngine = new GameEngine();
  * Run a single loop of the game.
  * @param {object}   data    the game state.
  */
-function mainLoop(data) {}
+function mainLoop(data) {
+  gameEngine.fillPixels([[0,0], [0, 1]], 255, 0, 0 , 255);
+}
 
 gameEngine.startMainLoop(mainLoop, {});
