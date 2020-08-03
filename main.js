bff775ff18b0cfd41dadd4cf426c294b7b6896af
RaySunlight <some.other.fairy@gmail.com>
2020-08-03 08:59:53 +0000

    לא מוחק לחלוטין

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
+//cupcake function
 "use strict";
-/*
-elformation: has information for Elephant.
-ele_show: show elephant
-use function like this:
-
-ele_show(elformation.x, elformation.y, elformation.r, elformation.g, elformation.b, elformation.a, gameEngine);
-then change vars in elformation
-...
-
-*/
 const gameEngine = new GameEngine();
 
-//information for elephant
-let elformation = {
-  //place
-  x:20,
-  y:20,
-  //color
-  r:0,
-  g:0,
-  b:255,
-  a:255
-}
-
-//showing the elephant
-function ele_show(x, y, r, g, b, a, gameEngine) {
+function cupcake_show(x, y, gameEngine) {
   let position =[];
 
   for (let i=0; i<15; i+=1) {
@@ -40,12 +18,10 @@ function ele_show(x, y, r, g, b, a, gameEngine) {
     }
   }
 
-  gameEngine.fillPixels(position, r, g, b, a);
+  gameEngine.fillPixels(position, 170, 70, 210, 255);
 }
 
-//un-showing old elephant
-//showing the elephant
-function ele_delete(x, y, gameEngine) {
+function cupcake_delete(x, y, gameEngine) {
   let position =[];
 
   for (let i=0; i<15; i+=1) {
@@ -61,56 +37,32 @@ function ele_delete(x, y, gameEngine) {
     }
   }
 
-  gameEngine.fillPixels(position, 0, 0, 0, 0);
+  gameEngine.fillPixels(position, 170, 70, 210, 80);
 }
 
-//movment- checks to where and modify. need to write in a way that will after show new elephant
-function moveElephent()
-{
-  if(gameEngine.isKeyHeld("ArrowRight"))
-  {
-    elformation.x+=1;
-  }
-
-  if(gameEngine.isKeyHeld("ArrowLeft"))
-  {
-     elformation.x-=1;
-  }
-
-  if(gameEngine.isKeyHeld("ArrowUp"))
-  {
-    elformation.y+=1;
-  }
-
-  if(gameEngine.isKeyHeld("ArrowDown"))
-  {
-    elformation.x-=1;
-  }  
+let cupdinate = {
+  x:25,
+  y:25
 }
 
-class Elephant
-{
-  function colorRed()
-  {
-    if(this.color.r!==255)
-    {
-      this.color.r+=5.1;
-      this.color.b-=5.1;
-    }
+function rancake() {
+  let ranx = Math.floor(Math.random() * 860);
+  let rany = Math.floor(Math.random() * 478);
+  return [ranx, rany];
+}
+
+function mainLoop(data) {
+  if (gameEngine.isKeyHeld(" ")) {
+    cupcake_delete(cupdinate.x, cupdinate.y, gameEngine)
+    let new1 = rancake();
+    cupdinate.x = new1[0];
+    cupdinate.y = new1[1];
+    cupcake_show(cupdinate.x, cupdinate.y, gameEngine);
   }
 
-  function colorBlue()
-  {
-    if(this.color.b!==255)
-    {
-      this.color.r-=5.1;
-      this.color.b+=5.1;
-    }
+  else {
+      cupcake_show(cupdinate.x, cupdinate.y, gameEngine);
   }
 }
 
-function mainLoop(data) 
-{
-  let e1 = Elephant.constructor(10,10,10);
-  elephant_show(gameEngine);
-}
+gameEngine.startMainLoop(mainLoop, {});
